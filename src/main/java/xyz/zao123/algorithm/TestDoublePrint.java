2d15a8d8f252b9930f8c9492dbca9cd294f9768f
gejt <852442493@qq.com>
2020-12-21 21:46:40 +0800

    annotation

--- src/main/java/xyz/zao123/algorithm/TestDoublePrint.java
+++ src/main/java/xyz/zao123/algorithm/TestDoublePrint.java
@@ -1,5 +1,10 @@
 package xyz.zao123.algorithm;
 
+/**
+ * 双线程交替打印出数字和字母 例如：A1B2C3D4E5
+ * @author  gejt
+ * @date 20201221
+ */
 public class TestDoublePrint {
 
     public static void main(String[] args){
